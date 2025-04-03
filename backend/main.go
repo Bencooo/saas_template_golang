eee8f90ff84634f5f6512050365c3201e41bed4f
Bencooo <130862404+Bencooo@users.noreply.github.com>
2025-04-03 13:13:11 +0200

    create docker compose, with database mongo (#1)

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	// env := os.Getenv("BECKEND_URL")
+	// fmt.Println(env)
+
+	ConnectDatabase()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Online backend")
+	})
+
+	fmt.Println("Start Server on 8080")
+	http.ListenAndServe(":8080", nil)
 }
